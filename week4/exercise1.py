1c3bb67103f6055c8de9322e7569ec59dcd7d69b
hedgytrimmer <jwsjt1@gmail.com>
2020-06-24 23:17:36 +1000

    Test

--- week4/exercise1.py
+++ week4/exercise1.py
@@ -94,27 +94,36 @@ def wordy_pyramid():
         lst.append(str(word2))
     return lst
 
-    def pokedex(low=1, high=5):
-        """ Return the name, height and weight of the tallest pokemon in the range low to high.
+def pokedex(low=1, high=5):
+    """ Return the name, height and weight of the tallest pokemon in the range low to high.
 
-        Low and high are the range of pokemon ids to search between.
-        Using the Pokemon API: https://pokeapi.co get some JSON using the request library
-        (a working example is filled in below).
-        Parse the json and extract the values needed.
-        
-        TIP: reading json can someimes be a bit confusing. Use a tool like
-            http://www.jsoneditoronline.org/ to help you see what's going on.
-        TIP: these long json accessors base["thing"]["otherThing"] and so on, can
-            get very long. If you are accessing a thing often, assign it to a
-            variable and then future access will be easier.
-        """
+    Low and high are the range of pokemon ids to search between.
+    Using the Pokemon API: https://pokeapi.co get some JSON using the request library
+    (a working example is filled in below).
+    Parse the json and extract the values needed.
+    
+    TIP: reading json can someimes be a bit confusing. Use a tool like
+        http://www.jsoneditoronline.org/ to help you see what's going on.
+    TIP: these long json accessors base["thing"]["otherThing"] and so on, can
+        get very long. If you are accessing a thing often, assign it to a
+        variable and then future access will be easier.
+    """
     template = "https://pokeapi.co/api/v2/pokemon/{id}"
-
-    url = template.format(id=5)
-    r = requests.get(url)
-    if r.status_code is 200:
-        the_json = json.loads(r.text)
-    return {"name": None, "weight": None, "height": None}
+    name = 0
+    height = 0
+    weight = 0
+    for i in range(low,high):
+        url = template.format(id=i)
+        r = requests.get(url)
+        if r.status_code is 200:
+            the_json = json.loads(r.text)
+            if the_json["height"] > height:
+                name = the_json["name"]
+                weight = the_json["weight"]
+                height = the_json["height"]
+            else:
+                continue
+    return {"name": name , "weight": weight, "height": height}
 
 
 def diarist():
@@ -131,7 +140,23 @@ def diarist():
          the test will have nothing to look at.
     TIP: this might come in handy if you need to hack a 3d print file in the future.
     """
-    pass
+    file_path = LOCAL + "\Trispokedovetiles(laser).gcode"
+    mode = "r"
+    number = 0
+    #x = open(file_path, mode)
+    lasers = open(file_path, mode).readlines()
+    #data.close()
+    for i in lasers:
+        print(i)
+        if "M10 P1" in i:
+            number += 1
+    #number = lasers.count("M10 P1")
+    lst = open("lasers.pew", "w")
+    lst.write(str(number))
+    lst.close
+
+
+
 
 
 if __name__ == "__main__":
